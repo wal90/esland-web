745d6230bda5497c346cdf15d00a9546556246d1
wal90 <walteromartinez90@gmail.com>
2024-01-19 22:40:24 -0300

    i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
+    i18n: {
+        defaultLocale: 'es',
+        locales: ['es', 'ca']
+    }
 });
