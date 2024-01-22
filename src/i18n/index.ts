43a0a444d8e9285252ebd8a7c787466907905a81
wal90 <walteromartinez90@gmail.com>
2024-01-22 19:49:55 -0300

    languages

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,6 +1,11 @@
 import catalan from './ca.json'
 import spanish from './es.json'
 
+const LANGUAGES = {
+    CATALAN: 'ca',
+    SPANISH: 'es'
+}
+
 export const getI18N = ({ currentLocale }) => {
     
 }
