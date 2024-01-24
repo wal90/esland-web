2427ee682175eb2c5d39da9d03f82551632fda20
wal90 <walteromartinez90@gmail.com>
2024-01-23 21:53:17 -0300

    current locale string

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -6,7 +6,7 @@ const LANGUAGES = {
     SPANISH: 'es'
 }
 
-export const getI18N = ({ currentLocale } : { currentLocale: 'es' | 'ca'}) => {
+export const getI18N = ({ currentLocale } : { currentLocale: string }) => {
     if (currentLocale === LANGUAGES.CATALAN) return catalan
     if (currentLocale === LANGUAGES.SPANISH) return spanish
     return spanish
