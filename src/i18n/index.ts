a7fae7e4c6f47ee7baf780378715bd8b67cf304e
wal90 <walteromartinez90@gmail.com>
2024-01-23 21:58:48 -0300

    current locale undefined

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -6,7 +6,7 @@ const LANGUAGES = {
     SPANISH: 'es'
 }
 
-export const getI18N = ({ currentLocale = 'es' } : { currentLocale ?: string }) => {
+export const getI18N = ({ currentLocale = 'es' } : { currentLocale: string | undefined }) => {
     if (currentLocale === LANGUAGES.CATALAN) return catalan
     if (currentLocale === LANGUAGES.SPANISH) return spanish
     return spanish
